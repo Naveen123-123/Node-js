f5f28fbc71eb4291ca5096c6a32d0b03ceda65e6
Naveen.Patibandla <Naveen.Patibandla@vistex.com>
2026-05-19 16:36:46 +0530

    HTML Templating

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -32,22 +32,69 @@ const http = require("http");
 // });
 // console.log("Reading file asynchronously");
 
+// Helper functions
+const replacePlaceholder = (tempCard, product) => {
+  let output = tempCard.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+// Reading Data
+const data = fs.readFileSync(`./starter/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+const tempOverview = fs.readFileSync(
+  `./starter/templates/overview.html`,
+  "utf-8",
+);
+const tempProduct = fs.readFileSync(
+  `./starter/templates/product.html`,
+  "utf-8",
+);
+const tempCard = fs.readFileSync(`./starter/templates/card.html`, "utf-8");
+
 // Server
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  if(pathName === "/" || pathName === "/overview"){
-    res.end("This is the overview");
-  }else if(pathName === "/product"){
+  // Overview Page
+  if (pathName === "/" || pathName === "/overview") {
+    const cardsHtml = dataObj
+      .map((el) => replacePlaceholder(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCTS_CARDS%}", cardsHtml);
+    res.writeHead(200, {
+       "Content-Type": "text/html",
+    });
+    res.end(output);
+  }
+  // Product Page
+  else if (pathName === "/product") {
     res.end("This is the product");
-  }else{
+  }
+
+  // API Page
+  else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify(dataObj));
+  }
+
+  // Not Found
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello world",
     });
-     res.end("<h1>Page not found!</h1>");
+    res.end("<h1>Page not found!</h1>");
   }
 });
 
-server.listen(8000,"127.0.0.1",()=>{
+server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
-})
+});
