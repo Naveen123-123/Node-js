e63b87417309e326aa9564f2b4ca3954ccd6e680
Naveen.Patibandla <Naveen.Patibandla@vistex.com>
2026-05-23 12:54:57 +0530

    module structure implementation

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
-const url = require('url');
+const url = require("url");
+const replacePlaceholder = require("./starter/modules/replaceTemplate")
 // Blocking, synchronous way
 // Reading from the file
 // FileSystem
@@ -33,21 +34,6 @@ const url = require('url');
 // });
 // console.log("Reading file asynchronously");
 
-// Helper functions
-const replacePlaceholder = (tempCard, product) => {
-  let output = tempCard.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%NEUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
-
 // Reading Data
 const data = fs.readFileSync(`./starter/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
@@ -64,7 +50,6 @@ const tempCard = fs.readFileSync(`./starter/templates/card.html`, "utf-8");
 // Server
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url,true);
-  console.log(query,pathname);
   // Overview Page
   if (pathname === "/" || pathname === "/overview") {
     const cardsHtml = dataObj
